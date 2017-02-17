c3680da3184f7cb3ca83e8dd0de9122ef6a53d73
Maria Ines Aranguren <marangu1@nd.edu>
2017-02-17 09:38:59 +0000

    Modified boolean functions identifying hand type using 5 as final sizeOfHand and using that in the implementations.

--- src/poker/HandOfCards.java
+++ src/poker/HandOfCards.java
@@ -1,13 +1,23 @@
 // Maria Ines Aranguren
-// Feb 15, 2017
-// Programming Assignment Week	#3: OOP Representation of a HandOfCards
 package poker;
+import java.lang.Math;
 
 public class HandOfCards {
 
     private PlayingCard[] hand;
     private final int sizeOfHand = 5;
     private DeckOfCards deck;
+
+    public int ROYAL_FLUSH_VAL = (int) (1*Math.pow(10,10));
+    public int STRAIGHT_FLUSH_VAL = (int) (1*Math.pow(10,9));
+    public int FOUR_OF_A_KIND_VAL = (int) (1*Math.pow(10,8));
+    public int FULL_HOUSE_VAL = (int) (1*Math.pow(10,7));
+    public int FLUSH_VAL = (int) (1*Math.pow(10,6));
+    public int STRAIGHT_VAL = (int) (1*Math.pow(10,5));
+    public int THREE_OF_A_KIND_VAL = (int) (1*Math.pow(10,4));
+    public int TWO_PAIR_VAL = (int) (1*Math.pow(10,3));
+    public int ONE_PAIR_VAL = (int) (1*Math.pow(10,2));
+    public int HIGH_HAND_VAL = (int) (1*Math.pow(10,1));
 
     public HandOfCards(DeckOfCards sourceDeck) {
         this.deck = sourceDeck;
@@ -42,157 +52,71 @@ public class HandOfCards {
     }
 
     public boolean isFourOfAKind() {                    // Contains four cards of the same rank and one card of another rank
-        if (sizeOfHand < 4) {                           // Checks if hand has enough cards to make FourOfAKind
-            return false;
-        }
-        for (int i = 0; i < sizeOfHand - 3; i++) {
-            if (this.hand[i].getGameVal() == this.hand[i + 1].getGameVal() && this.hand[i].getGameVal() == this.hand[i + 2].getGameVal() && this.hand[i].getGameVal() == this.hand[i + 3].getGameVal()) {
-                int j = 0;                              // Variable to iterate through hand to check for common rank between non-quad cards
-                if (i == 0) {                           // If quad starts with first card of hand, skip past four cards
-                    j = 4;
-                }
-                int nextNonQuadCard = j + 1;            // Index for next card that isn't part of the quad
-                while (j < sizeOfHand) {                // Loop through hand to verify that no cards share common rank
-                    if (nextNonQuadCard == i) {         // If nextNonQuadCard points to start of quad, increment by 4a to skip quad cards
-                        nextNonQuadCard += 4;
-                    }
-                    if (nextNonQuadCard < sizeOfHand) { // Check if nextNonQuadCard card points to card within the bounds of the hand
-                        if (this.hand[j].getGameVal() == this.hand[i].getGameVal() || this.hand[j].getGameVal() == this.hand[nextNonQuadCard].getGameVal()) {  // Checks if rank is equal to either rank of triplet or rank of nextNonQuadCard
-                            return false;
-                        }
-                        i = nextNonQuadCard;            // Update nextNonQuadCard to point to next card
-                        nextNonQuadCard++;
-                    } else {
-                        break;                          // If nextNonQuadCard is out of bounds, checking is complete
-                    }
-                }
-                return true;
-            }
+        if (this.hand[0].getGameVal() != this.hand[1].getGameVal() && this.hand[1].getGameVal() == this.hand[4].getGameVal()) {          // Case A: Different card is first card
+            return true;
+        } else if (this.hand[4].getGameVal() != this.hand[3].getGameVal() && this.hand[3].getGameVal() == this.hand[0].getGameVal()) {   // Case B: Different card is last card
+            return true;
         }
         return false;
     }
 
     public boolean isThreeOfAKind() {                   // Contains three cards of the same rank and two cards of two other ranks
-        if (sizeOfHand < 3) {                           // Checks if hand has enough cards to make ThreeOfAKind
-            return false;
-        }
-        for (int i = 0; i < sizeOfHand - 2; i++) {
-            if (this.hand[i].getGameVal() == this.hand[i + 1].getGameVal() && this.hand[i].getGameVal() == this.hand[i + 2].getGameVal()) {  // Checks if there are at least three cards of the same rank
-                int j = 0;                              // Variable to iterate through hand to check for common rank between non-triplet cards
-                if (i == 0) {                           // If triplet starts with first card of hand, skip past three cards
-                    j = 3;
-                }
-                int nextNonTripletCard = j + 1;          // Index for next card that isn't part of the triple
-                while (j < sizeOfHand) {                 // Loop through hand to verify that no cards share common rank
-                    if (nextNonTripletCard == i) {       // If nextNonTripletCard points to start of triplet, increment by 3 to skip triplet cards
-                        nextNonTripletCard += 3;
-                    }
-                    if (nextNonTripletCard < sizeOfHand) {  // Check if nextNonTriplet card points to card within the bounds of the hand
-                        if (this.hand[j].getGameVal() == this.hand[i].getGameVal() || this.hand[j].getGameVal() == this.hand[nextNonTripletCard].getGameVal()) {  // Checks if rank is equal to either rank of triplet or rank of nextNonTripletCard
-                            return false;
-                        }
-                        i = nextNonTripletCard;         // Update nextNonTripletCard to point to next card
-                        nextNonTripletCard++;
-                    } else {
-                        break;                          // If nextNonTripletCard is out of bounds, checking is complete
-                    }
-                }
-                return true;
-            }
+        if (this.hand[0].getGameVal() == this.hand[2].getGameVal() && this.hand[2].getGameVal() != this.hand[3].getGameVal() && this.hand[3].getGameVal() != this.hand[4].getGameVal()) {          // Case A: Three same-rank cards first, then two unmatched cards
+            return true;
+        } else if (this.hand[0].getGameVal() != this.hand[1].getGameVal() && this.hand[1].getGameVal() == this.hand[3].getGameVal() && this.hand[3].getGameVal() != this.hand[4].getGameVal()) {   // Case B: Unmatched card first, then three same-rank cards, then another unmatched card
+            return true;
+        } else if (this.hand[0].getGameVal() != this.hand[1].getGameVal() && this.hand[1].getGameVal() != this.hand[2].getGameVal() && this.hand[2].getGameVal() == this.hand[4].getGameVal()) {   // Case C: Two unmatched cards first, then three same-rank cards
+            return true;
         }
         return false;
     }
 
     public boolean isFullHouse() {                      // Contains three cards of one rank and two cards of another rank
-        if (sizeOfHand < 5) {                           // Checks if hand has enough cards to make FullHouse
-            return false;
-        }
-        for (int i = 0; i < sizeOfHand - 2; i++) {
-            if (this.hand[i].getGameVal() == this.hand[i + 1].getGameVal() && this.hand[i].getGameVal() == this.hand[i + 2].getGameVal()) {  // Checks if there are at least three cards of the same rank
-                int j = 0;                              // Variable to iterate through hand to check for common rank between non-triplet cards
-                if (i == 0) {                           // If triplet starts with first card of hand, skip past three cards
-                    j = 3;
-                }
-                int nextNonTripletCard = j + 1;          // Index for next card that isn't part of the triple
-                while (j < sizeOfHand) {                 // Loop through hand to verify that no cards share common rank
-                    if (nextNonTripletCard == i) {       // If nextNonTripletCard points to start of triplet, increment by 3 to skip triplet cards
-                        nextNonTripletCard += 3;
-                    }
-                    if (nextNonTripletCard < sizeOfHand) {  // Check if nextNonTriplet card points to card within the bounds of the hand
-                        if (this.hand[j].getGameVal() != this.hand[i].getGameVal() && this.hand[j].getGameVal() == this.hand[nextNonTripletCard].getGameVal()) {  // Checks if rank is equal to either rank of triplet or rank of nextNonTripletCard
-                            return true;
-                        }
-                        i = nextNonTripletCard;         // Update nextNonTripletCard to point to next card
-                        nextNonTripletCard++;
-                    } else {
-                        break;                          // If nextNonTripletCard is out of bounds, checking is complete
-                    }
-                }
-            }
+        if (this.hand[0].getGameVal() == this.hand[1].getGameVal() && this.hand[1].getGameVal() != this.hand[2].getGameVal() && this.hand[2].getGameVal() == this.hand[4].getGameVal()) {          // Case A: Two same-rank cards first, then three same-rank cards
+            return true;
+        } else if (this.hand[0].getGameVal() == this.hand[2].getGameVal() && this.hand[2].getGameVal() != this.hand[3].getGameVal() && this.hand[3].getGameVal() == this.hand[4].getGameVal()) {   // Case B: Three same-rank cards first, then two same-rank cards
+            return true;
         }
         return false;
     }
 
     public boolean isRoyalFlush() {           // An ace-high straight flush
-        if (sizeOfHand < 5) {                 // Checks if hand has enough cards to make RoyalFlush
-            return false;
-        }
-        for (int i = 0; i < sizeOfHand - 4; i++) {
-            if (this.hand[i].getGameVal() == 14 && this.hand[i + 1].getGameVal() == 13 && this.hand[i + 2].getGameVal() == 12 && this.hand[i + 3].getGameVal() == 11 && this.hand[i + 4].getGameVal() == 10) {                                            // Checks if cards are A, K, Q, J, 10
-                if (this.hand[i].getSuit() == this.hand[i + 1].getSuit() && this.hand[i].getSuit() == this.hand[i + 2].getSuit() && this.hand[i].getSuit() == this.hand[i + 3].getSuit() && this.hand[i].getSuit() == this.hand[i + 4].getSuit()) {     // Checks if cards are of the same suit
-                    return true;
-                }
+        if (this.hand[0].getGameVal() == 10 && this.hand[1].getGameVal() == 11 && this.hand[2].getGameVal() == 12 && this.hand[3].getGameVal() == 13 && this.hand[4].getGameVal() == 14) {                                            // Checks if cards are 10, J, Q, K, A
+            if (this.hand[0].getSuit() == this.hand[1].getSuit() && this.hand[0].getSuit() == this.hand[2].getSuit() && this.hand[0].getSuit() == this.hand[3].getSuit() && this.hand[0].getSuit() == this.hand[4].getSuit()) {   // Checks if cards are of the same suit
+                return true;
             }
         }
         return false;
     }
 
     public boolean isStraightFlush() {       // Contains five cards of sequential rank, all of the same suit
-        if (sizeOfHand < 5) {                // Checks if hand has enough cards to make StraightFlush
-            return false;
-        }
-        for (int i = 0; i < sizeOfHand - 4; i++) {
-            if (this.hand[i].getGameVal() == this.hand[i + 1].getGameVal() - 1 && this.hand[i + 1].getGameVal() == this.hand[i + 2].getGameVal() - 1 && this.hand[i + 2].getGameVal() == this.hand[i + 3].getGameVal() - 1 && this.hand[i + 3].getGameVal() == this.hand[i + 4].getGameVal() - 1) {   // Checks if cards are of sequential rank
-                if (this.hand[i].getSuit() == this.hand[i + 1].getSuit() && this.hand[i].getSuit() == this.hand[i + 2].getSuit() && this.hand[i].getSuit() == this.hand[i + 3].getSuit() && this.hand[i].getSuit() == this.hand[i + 4].getSuit()) {                                     // Checks if cards are of the same suit
-                    return true;
-                }
+        if (this.hand[0].getGameVal() == this.hand[1].getGameVal() - 1 && this.hand[1].getGameVal() == this.hand[2].getGameVal() - 1 && this.hand[2].getGameVal() == this.hand[3].getGameVal() - 1 && this.hand[3].getGameVal() == this.hand[4].getGameVal() - 1) { // Checks if cards are of sequential rank
+            if (this.hand[0].getSuit() == this.hand[1].getSuit() && this.hand[0].getSuit() == this.hand[2].getSuit() && this.hand[0].getSuit() == this.hand[3].getSuit() && this.hand[0].getSuit() == this.hand[4].getSuit()) {                                     // Checks if cards are of the same suit
+                return true;
             }
         }
         return false;
     }
 
     public boolean isStraight() {           // Contains five cards of sequential rank, not all of the same suit
-        if (sizeOfHand < 5) {               // Checks if hand has enough cards to make Straight
-            return false;
-        }
-        for (int i = 0; i < sizeOfHand - 4; i++) {
-            if (this.hand[i].getGameVal() == this.hand[i + 1].getGameVal() - 1 && this.hand[i + 1].getGameVal() == this.hand[i + 2].getGameVal() - 1 && this.hand[i + 2].getGameVal() == this.hand[i + 3].getGameVal() - 1 && this.hand[i + 3].getGameVal() == this.hand[i + 4].getGameVal() - 1) {           // Checks if all cards are sequential
-                if (!(this.hand[i].getSuit() == this.hand[i + 1].getSuit() && this.hand[i].getSuit() == this.hand[i + 2].getSuit() && this.hand[i].getSuit() == this.hand[i + 3].getSuit() && this.hand[i].getSuit() == this.hand[i + 4].getSuit())) {                                          // Checks if not all cards are of the same suit
-                    return true;
-                }
+        if (this.hand[0].getGameVal() == this.hand[1].getGameVal() - 1 && this.hand[1].getGameVal() == this.hand[2].getGameVal() - 1 && this.hand[2].getGameVal() == this.hand[3].getGameVal() - 1 && this.hand[3].getGameVal() == this.hand[4].getGameVal() - 1) { // Checks if all cards are sequential
+            if (this.hand[0].getSuit() != this.hand[1].getSuit() || this.hand[0].getSuit() != this.hand[2].getSuit() || this.hand[0].getSuit() != this.hand[3].getSuit() || this.hand[0].getSuit() != this.hand[4].getSuit()) {                                  // Checks if not all cards are of the same suit
+                return true;
             }
         }
         return false;
     }
 
     public boolean isFlush() {              // Contains five cards all of the same suit, not all of sequential rank
-        if (sizeOfHand < 5) {               // Checks if hand has enough cards to make Flush
-            return false;
-        }
-        for (int i = 0; i < sizeOfHand - 4; i++) {
-            if (this.hand[i].getSuit() == this.hand[i + 1].getSuit() && this.hand[i].getSuit() == this.hand[i + 2].getSuit() && this.hand[i].getSuit() == this.hand[i + 3].getSuit() && this.hand[i].getSuit() == this.hand[i + 4].getSuit()) {                                                 // Checks if all cards are of the same suit
-                if (!(this.hand[i].getGameVal() == this.hand[i + 1].getGameVal() - 1 && this.hand[i + 1].getGameVal() == this.hand[i + 2].getGameVal() - 1 && this.hand[i + 2].getGameVal() == this.hand[i + 3].getGameVal() - 1 && this.hand[i + 3].getGameVal() == this.hand[i + 4].getGameVal() - 1)) {    // Checks if not all cards are sequential
-                    return true;
-                }
+        if (this.hand[0].getSuit() == this.hand[1].getSuit() && this.hand[0].getSuit() == this.hand[2].getSuit() && this.hand[0].getSuit() == this.hand[3].getSuit() && this.hand[0].getSuit() == this.hand[4].getSuit()) {                                             // Checks if all cards are of the same suit
+            if (this.hand[0].getGameVal() != this.hand[1].getGameVal() - 1 || this.hand[1].getGameVal() != this.hand[2].getGameVal() - 1 || this.hand[2].getGameVal() != this.hand[3].getGameVal() - 1 || this.hand[3].getGameVal() != this.hand[4].getGameVal() - 1) { // Checks if not all cards are sequential
+                return true;
             }
         }
         return false;
     }
 
     public boolean isTwoPair() {            // Contains two cards of the same rank, two cards of another rank and one card of a third rank
-        if (sizeOfHand < 4) {               // Checks if hand has enough cards to make TwoPair
-            return false;
-        }
         int pairsFound = 0;
         int rankPairA = 0;
         int rankPairB = 0;
@@ -206,26 +130,17 @@ public class HandOfCards {
                 }
             }
         }
-        if (pairsFound == 2 && rankPairA != rankPairB) {  // Checks if found two pairs of different rank (also covers the case that they're three cards of a kind)
-            return true;
-        }
-        return false;
+        return (pairsFound == 2 && rankPairA != rankPairB);
     }
 
     public boolean isOnePair() {            // Contains two cards of the same rank and three cards of three other ranks
-        if (sizeOfHand < 2) {               // Checks if hand has enough cards to make OnePair
-            return false;
-        }
         int pairsFound = 0;                                                         // Leeps track of number of pairs found so that function returns true only when exactly one pair is found (false when there are no pairs, more than one pair, threeOfAKind, and fourOfAkind)
         for (int i = 0; i < sizeOfHand - 1; i++) {                                  // Loops through hand and checks adjacent cards to see if they're a pair
             if (this.hand[i].getGameVal() == this.hand[i + 1].getGameVal()) {
                 pairsFound++;                                                       // Updates pairsFound to reflect new pairsFound
             }
         }
-        if (pairsFound == 1) {
-            return true;
-        }
-        return false;
+        return (pairsFound == 1);
     }
 
     public boolean isHighHand() {
