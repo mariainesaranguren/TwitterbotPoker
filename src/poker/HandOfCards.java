2f620ffd5e7c815510da169672304f4eaa52b5a7
Maria Ines Aranguren <marangu1@nd.edu>
2017-02-22 11:43:11 +0000

    Added replaceCard() method for testing code in Runner.java for testing getGameValue() method of HandOfCards

--- src/poker/HandOfCards.java
+++ src/poker/HandOfCards.java
@@ -1,35 +1,39 @@
 // Maria Ines Aranguren
 package poker;
+
 import java.lang.Math;
 
 public class HandOfCards {
 
     private PlayingCard[] hand;
-    private final int sizeOfHand = 5;
+    private final int SIZE_OF_HAND = 5;
     private DeckOfCards deck;
 
-    public int ROYAL_FLUSH_VAL = (int) (1*Math.pow(10,10));
-    public int STRAIGHT_FLUSH_VAL = (int) (1*Math.pow(10,9));
-    public int FOUR_OF_A_KIND_VAL = (int) (1*Math.pow(10,8));
-    public int FULL_HOUSE_VAL = (int) (1*Math.pow(10,7));
-    public int FLUSH_VAL = (int) (1*Math.pow(10,6));
-    public int STRAIGHT_VAL = (int) (1*Math.pow(10,5));
-    public int THREE_OF_A_KIND_VAL = (int) (1*Math.pow(10,4));
-    public int TWO_PAIR_VAL = (int) (1*Math.pow(10,3));
-    public int ONE_PAIR_VAL = (int) (1*Math.pow(10,2));
-    public int HIGH_HAND_VAL = (int) (1*Math.pow(10,1));
+    public static final int ROYAL_FLUSH_VAL = (int) (1 * Math.pow(10, 10));     // Default value of hand increases by factor of 10
+                                                                                // between each ranking to ensure that overall game value
+                                                                                // of card will correspondingly represent its value and be
+                                                                                // appropriate for comparison with other hands. 
+    public static final int STRAIGHT_FLUSH_VAL = (int) (1 * Math.pow(10, 9));
+    public static final int FOUR_OF_A_KIND_VAL = (int) (1 * Math.pow(10, 8));
+    public static final int FULL_HOUSE_VAL = (int) (1 * Math.pow(10, 7));
+    public static final int FLUSH_VAL = (int) (1 * Math.pow(10, 6));
+    public static final int STRAIGHT_VAL = (int) (1 * Math.pow(10, 5));
+    public static final int THREE_OF_A_KIND_VAL = (int) (1 * Math.pow(10, 4));
+    public static final int TWO_PAIR_VAL = (int) (1 * Math.pow(10, 3));
+    public static final int ONE_PAIR_VAL = (int) (1 * Math.pow(10, 2));
+    public static final int HIGH_HAND_VAL = (int) (1 * Math.pow(10, 1));
 
     public HandOfCards(DeckOfCards sourceDeck) {
         this.deck = sourceDeck;
-        this.hand = new PlayingCard[this.sizeOfHand];   // Instantiates hand by allocating an array of sizeOfHand PlayingCards
-        for (int i = 0; i < this.sizeOfHand; i++) {     // Iterates through hand and deals sizeOfHand cards
+        this.hand = new PlayingCard[this.SIZE_OF_HAND];   // Instantiates hand by allocating an array of SIZE_OF_HAND PlayingCards
+        for (int i = 0; i < this.SIZE_OF_HAND; i++) {     // Iterates through hand and deals SIZE_OF_HAND cards
             this.hand[i] = deck.dealNext();
         }
         this.sort();                                    // Sorts cards
     }
 
     public void print() {                               // Iterates through hand and prints string representation of cards
-        for (int i = 0; i < sizeOfHand; i++) {
+        for (int i = 0; i < SIZE_OF_HAND; i++) {
             System.out.printf("%s ", hand[i].toString());
         }
         System.out.printf("\n");
@@ -40,7 +44,7 @@ public class HandOfCards {
     }
 
     private void sort() {                               // Sorts hand 2-A disregarding suits
-        for (int i = 0; i < sizeOfHand - 1; i++) {      // Performs insertion sort on the hand of cards
+        for (int i = 0; i < SIZE_OF_HAND - 1; i++) {      // Performs insertion sort on the hand of cards
             int j = i + 1;
             PlayingCard tmp = this.hand[j];
             while (j > 0 && tmp.getGameVal() < this.hand[j - 1].getGameVal()) {
@@ -92,7 +96,9 @@ public class HandOfCards {
     public boolean isStraightFlush() {       // Contains five cards of sequential rank, all of the same suit
         if (this.hand[0].getGameVal() == this.hand[1].getGameVal() - 1 && this.hand[1].getGameVal() == this.hand[2].getGameVal() - 1 && this.hand[2].getGameVal() == this.hand[3].getGameVal() - 1 && this.hand[3].getGameVal() == this.hand[4].getGameVal() - 1) { // Checks if cards are of sequential rank
             if (this.hand[0].getSuit() == this.hand[1].getSuit() && this.hand[0].getSuit() == this.hand[2].getSuit() && this.hand[0].getSuit() == this.hand[3].getSuit() && this.hand[0].getSuit() == this.hand[4].getSuit()) {                                     // Checks if cards are of the same suit
-                return true;
+                if (!this.isRoyalFlush()) {
+                    return true;
+                }
             }
         }
         return false;
@@ -110,7 +116,9 @@ public class HandOfCards {
     public boolean isFlush() {              // Contains five cards all of the same suit, not all of sequential rank
         if (this.hand[0].getSuit() == this.hand[1].getSuit() && this.hand[0].getSuit() == this.hand[2].getSuit() && this.hand[0].getSuit() == this.hand[3].getSuit() && this.hand[0].getSuit() == this.hand[4].getSuit()) {                                             // Checks if all cards are of the same suit
             if (this.hand[0].getGameVal() != this.hand[1].getGameVal() - 1 || this.hand[1].getGameVal() != this.hand[2].getGameVal() - 1 || this.hand[2].getGameVal() != this.hand[3].getGameVal() - 1 || this.hand[3].getGameVal() != this.hand[4].getGameVal() - 1) { // Checks if not all cards are sequential
-                return true;
+                if (!this.isFullHouse()) {
+                    return true;
+                }
             }
         }
         return false;
@@ -120,7 +128,7 @@ public class HandOfCards {
         int pairsFound = 0;
         int rankPairA = 0;
         int rankPairB = 0;
-        for (int i = 0; i < sizeOfHand - 1; i++) {
+        for (int i = 0; i < SIZE_OF_HAND - 1; i++) {
             if (this.hand[i].getGameVal() == this.hand[i + 1].getGameVal()) {   // Check neighboring cards to see if they're a pair
                 pairsFound++;
                 if (pairsFound == 1) {
@@ -135,7 +143,7 @@ public class HandOfCards {
 
     public boolean isOnePair() {            // Contains two cards of the same rank and three cards of three other ranks
         int pairsFound = 0;                                                         // Leeps track of number of pairs found so that function returns true only when exactly one pair is found (false when there are no pairs, more than one pair, threeOfAKind, and fourOfAkind)
-        for (int i = 0; i < sizeOfHand - 1; i++) {                                  // Loops through hand and checks adjacent cards to see if they're a pair
+        for (int i = 0; i < SIZE_OF_HAND - 1; i++) {                                  // Loops through hand and checks adjacent cards to see if they're a pair
             if (this.hand[i].getGameVal() == this.hand[i + 1].getGameVal()) {
                 pairsFound++;                                                       // Updates pairsFound to reflect new pairsFound
             }
@@ -148,6 +156,83 @@ public class HandOfCards {
             return false;
         }
         return true;
+    }
+
+    public int getGameValue() {
+        int generalHandVal = 0;
+        if (this.isHighHand()) {
+            generalHandVal = HIGH_HAND_VAL;
+        } else if (this.isOnePair()) {
+            generalHandVal = ONE_PAIR_VAL;
+        } else if (this.isTwoPair()) {
+            generalHandVal = TWO_PAIR_VAL;
+        } else if (this.isThreeOfAKind()) {
+            generalHandVal = THREE_OF_A_KIND_VAL;
+        } else if (this.isStraight()) {
+            generalHandVal = STRAIGHT_VAL;
+        } else if (this.isFlush()) {
+            generalHandVal = FLUSH_VAL;
+        } else if (this.isFullHouse()) {
+            generalHandVal = FULL_HOUSE_VAL;
+        } else if (this.isFourOfAKind()) {
+            generalHandVal = FOUR_OF_A_KIND_VAL;
+        } else if (this.isStraightFlush()) {
+            generalHandVal = STRAIGHT_FLUSH_VAL;
+        } else if (this.isRoyalFlush()) {
+            generalHandVal = ROYAL_FLUSH_VAL;
+        }
+
+        int totalVal = generalHandVal;                        // totalVal = generalHandVal + value of each card
+        int repeatedCardCount = 1;
+        for (int i = 0; i < SIZE_OF_HAND; i++) {              // Loop through cards and specific value of each card
+            while (i + 1 < SIZE_OF_HAND && this.hand[i].getGameVal() == this.hand[i + 1].getGameVal()) {
+                i++;
+                repeatedCardCount++;
+            }
+            totalVal += Math.pow(this.hand[i].getGameVal(), repeatedCardCount);
+            repeatedCardCount = 1;
+        }
+
+        return totalVal;
+    }
+    
+    public void identifyHand() {
+        this.sort();
+        if (this.isFlush()) {
+            System.out.printf("The hand is a Flush!\n");
+        }
+        if (this.isFourOfAKind()) {                                 // Intentionally not using else-ifs to test for correctness of functions
+            System.out.printf("The hand is a FourOfAKind!\n");
+        }
+        if (this.isFullHouse()) {
+            System.out.printf("The hand is a FullHouse!\n");
+        }
+        if (this.isHighHand()) {
+            System.out.printf("The hand is a HighHand!\n");
+        }
+        if (this.isOnePair()) {
+            System.out.printf("The hand is a OnePair!\n");
+        }
+        if (this.isRoyalFlush()) {
+            System.out.printf("The hand is a RoyalFlush!\n");
+        }
+        if (this.isStraight()) {
+            System.out.printf("The hand is a Straight!\n");
+        }
+        if (this.isStraightFlush()) {
+            System.out.printf("The hand is a StraightFlush!\n");
+        }
+        if (this.isThreeOfAKind()) {
+            System.out.printf("The hand is a ThreeOfAKind!\n");
+        }
+        if (this.isTwoPair()) {
+            System.out.printf("The hand is a TwoPair!\n");
+        }
+    }
+    
+    public void replaceCard(PlayingCard card, int location) {    // Created solely for testing purposes to allow for creation of specific poker hand 
+                                                                 // TODO: Remove after assignment #4 
+        this.hand[location] = card;
     }
 
 }
