f61091dad6bca466db510e458c22cda5a0ddfce8
Maria Ines Aranguren <marangu1@nd.edu>
2017-03-02 16:24:08 +0000

    Added getDiscardProbability() and accompanying methods

--- src/poker/HandOfCards.java
+++ src/poker/HandOfCards.java
@@ -7,21 +7,32 @@ public class HandOfCards {
 
     private PlayingCard[] hand;
     private final int SIZE_OF_HAND = 5;
+    private final int PROBABILITY_FACTOR = 1000;    // Used to expand probability scores by multiplying them by this factor (so that the scores are spread farther apart and they're not decimal values)
     private DeckOfCards deck;
 
-    public static final int ROYAL_FLUSH_VAL = (int) (1 * Math.pow(10, 10));     // Default value of hand increases by factor of 10
-                                                                                // between each ranking to ensure that overall game value
-                                                                                // of card will correspondingly represent its value and be
-                                                                                // appropriate for comparison with other hands. 
-    public static final int STRAIGHT_FLUSH_VAL = (int) (1 * Math.pow(10, 9));
-    public static final int FOUR_OF_A_KIND_VAL = (int) (1 * Math.pow(10, 8));
-    public static final int FULL_HOUSE_VAL = (int) (1 * Math.pow(10, 7));
-    public static final int FLUSH_VAL = (int) (1 * Math.pow(10, 6));
-    public static final int STRAIGHT_VAL = (int) (1 * Math.pow(10, 5));
-    public static final int THREE_OF_A_KIND_VAL = (int) (1 * Math.pow(10, 4));
-    public static final int TWO_PAIR_VAL = (int) (1 * Math.pow(10, 3));
-    public static final int ONE_PAIR_VAL = (int) (1 * Math.pow(10, 2));
-    public static final int HIGH_HAND_VAL = (int) (1 * Math.pow(10, 1));
+    private final String ROYAL_FLUSH_STR = "royalFlush";
+    private final String STRAIGHT_FLUSH_STR = "straightFlush";
+    private final String FOUR_OF_A_KIND_STR = "fourOfAKind";
+    private final String FULL_HOUSE_STR = "fullHouse";
+    private final String FLUSH_STR = "flush";
+    private final String STRAIGHT_STR = "straight";
+    private final String THREE_OF_A_KIND_STR = "threeOfAKind";
+    private final String TWO_PAIRS_STR = "twoPairs";
+    private final String ONE_PAIR_STR = "onePair";
+    private final String HIGH_STR = "high";
+
+    // Default value of hands to allow for comparison of value between rankinds.
+    // Increases by factor of 10 between each ranking to ensure that overall game value of card will correspondingly represent its value allowing hand type to take precedence over specific hands. 
+    public static final int ROYAL_FLUSH_DFLT_VAL = (int) (1 * Math.pow(10, 10));
+    public static final int STRAIGHT_FLUSH_DFLT_VAL = (int) (1 * Math.pow(10, 9));
+    public static final int FOUR_OF_A_KIND_DFLT_VAL = (int) (1 * Math.pow(10, 8));
+    public static final int FULL_HOUSE_DFLT_VAL = (int) (1 * Math.pow(10, 7));
+    public static final int FLUSH_DFLT_VAL = (int) (1 * Math.pow(10, 6));
+    public static final int STRAIGHT_DFLT_VAL = (int) (1 * Math.pow(10, 5));
+    public static final int THREE_OF_A_KIND_DFLT_VAL = (int) (1 * Math.pow(10, 4));
+    public static final int TWO_PAIR_DFLT_VAL = (int) (1 * Math.pow(10, 3));
+    public static final int ONE_PAIR_DFLT_VAL = (int) (1 * Math.pow(10, 2));
+    public static final int HIGH_HAND_DFLT_VAL = (int) (1 * Math.pow(10, 1));
 
     public HandOfCards(DeckOfCards sourceDeck) {
         this.deck = sourceDeck;
@@ -33,6 +44,8 @@ public class HandOfCards {
     }
 
     public void print() {                               // Iterates through hand and prints string representation of cards
+        this.sort();
+        System.out.printf("\t");
         for (int i = 0; i < SIZE_OF_HAND; i++) {
             System.out.printf("%s ", hand[i].toString());
         }
@@ -41,6 +54,14 @@ public class HandOfCards {
 
     public DeckOfCards returnSourceDeck() {
         return deck;
+    }
+
+    public PlayingCard[] getHand() {
+        return this.hand;
+    } 
+    
+    public int getSIZE_OF_HAND() {
+        return SIZE_OF_HAND;
     }
 
     private void sort() {                               // Sorts hand 2-A disregarding suits
@@ -161,25 +182,25 @@ public class HandOfCards {
     public int getGameValue() {
         int generalHandVal = 0;
         if (this.isHighHand()) {
-            generalHandVal = HIGH_HAND_VAL;
+            generalHandVal = HIGH_HAND_DFLT_VAL;
         } else if (this.isOnePair()) {
-            generalHandVal = ONE_PAIR_VAL;
+            generalHandVal = ONE_PAIR_DFLT_VAL;
         } else if (this.isTwoPair()) {
-            generalHandVal = TWO_PAIR_VAL;
+            generalHandVal = TWO_PAIR_DFLT_VAL;
         } else if (this.isThreeOfAKind()) {
-            generalHandVal = THREE_OF_A_KIND_VAL;
+            generalHandVal = THREE_OF_A_KIND_DFLT_VAL;
         } else if (this.isStraight()) {
-            generalHandVal = STRAIGHT_VAL;
+            generalHandVal = STRAIGHT_DFLT_VAL;
         } else if (this.isFlush()) {
-            generalHandVal = FLUSH_VAL;
+            generalHandVal = FLUSH_DFLT_VAL;
         } else if (this.isFullHouse()) {
-            generalHandVal = FULL_HOUSE_VAL;
+            generalHandVal = FULL_HOUSE_DFLT_VAL;
         } else if (this.isFourOfAKind()) {
-            generalHandVal = FOUR_OF_A_KIND_VAL;
+            generalHandVal = FOUR_OF_A_KIND_DFLT_VAL;
         } else if (this.isStraightFlush()) {
-            generalHandVal = STRAIGHT_FLUSH_VAL;
+            generalHandVal = STRAIGHT_FLUSH_DFLT_VAL;
         } else if (this.isRoyalFlush()) {
-            generalHandVal = ROYAL_FLUSH_VAL;
+            generalHandVal = ROYAL_FLUSH_DFLT_VAL;
         }
 
         int totalVal = generalHandVal;                        // totalVal = generalHandVal + value of each card
@@ -195,44 +216,132 @@ public class HandOfCards {
 
         return totalVal;
     }
-    
-    public void identifyHand() {
+
+    public String identifyHand() {
         this.sort();
         if (this.isFlush()) {
-            System.out.printf("The hand is a Flush!\n");
+            return FLUSH_STR;
         }
         if (this.isFourOfAKind()) {                                 // Intentionally not using else-ifs to test for correctness of functions
-            System.out.printf("The hand is a FourOfAKind!\n");
+            return FOUR_OF_A_KIND_STR;
         }
         if (this.isFullHouse()) {
-            System.out.printf("The hand is a FullHouse!\n");
+            return FULL_HOUSE_STR;
         }
         if (this.isHighHand()) {
-            System.out.printf("The hand is a HighHand!\n");
+            return HIGH_STR;
         }
         if (this.isOnePair()) {
-            System.out.printf("The hand is a OnePair!\n");
+            return ONE_PAIR_STR;
         }
         if (this.isRoyalFlush()) {
-            System.out.printf("The hand is a RoyalFlush!\n");
+            return ROYAL_FLUSH_STR;
         }
         if (this.isStraight()) {
-            System.out.printf("The hand is a Straight!\n");
+            return STRAIGHT_STR;
         }
         if (this.isStraightFlush()) {
-            System.out.printf("The hand is a StraightFlush!\n");
+            return STRAIGHT_FLUSH_STR;
         }
         if (this.isThreeOfAKind()) {
-            System.out.printf("The hand is a ThreeOfAKind!\n");
+            return THREE_OF_A_KIND_STR;
         }
         if (this.isTwoPair()) {
-            System.out.printf("The hand is a TwoPair!\n");
+            return TWO_PAIRS_STR;
         }
+        return null;
     }
-    
+
     public void replaceCard(PlayingCard card, int location) {    // Created solely for testing purposes to allow for creation of specific poker hand 
-                                                                 // TODO: Remove after assignment #4 
+        // TODO: Remove before production. This is here for testing for now.
         this.hand[location] = card;
     }
 
+    private HandOfCards[] copyNonDiscardingCards(int cardPosition, int totalPossibleHands) {
+        int cardsInserted = 0;
+        HandOfCards[] possibleHands = new HandOfCards[totalPossibleHands];      // Instantiate an array of hands to hold all other possible hands replacing specified card with all other possible cards (one row for each hand, SIZE_OF_HAND (5) columns/cards for each hand)
+        HandOfCards newHand = new HandOfCards(new DeckOfCards());               // Instantiate hand to assign to hands in possibleHands to intialize properly (cards within it will be overwritten as all hands possible to make when replacing discarded card).
+
+        // Copy over all cards in hand to all the hands in the array (possibleHands) except for specified card
+        for (int handIndex = 0; handIndex < totalPossibleHands; handIndex++) {    // Loop through all hands
+            possibleHands[handIndex] = new HandOfCards(new DeckOfCards());
+            cardsInserted = 0;                      // Reset to 0 so that it will work properly each time around loop for all hands
+            for (int cardIndex = 0; cardIndex < SIZE_OF_HAND; cardIndex++) {                                        // Copy over all hands except the one to be discarded
+                if (cardIndex != cardPosition) {
+                    possibleHands[handIndex].hand[cardsInserted] = null;
+                    possibleHands[handIndex].hand[cardsInserted] = this.hand[cardIndex];
+                    cardsInserted++;
+                }
+            }
+        }
+        return possibleHands;
+    }
+
+    private HandOfCards[] completeHandsWithDeck(HandOfCards[] possibleHands, int totalPossibleHands) {
+        DeckOfCards checkDeck = new DeckOfCards();                              // Use this deck to see which cards are not in the hand and assume them to be in the deck
+        boolean foundMissing = true;
+        int handsCompleted = 0;
+        int lastPosition = 4;           // First 4 cards should already be placed.
+        for (int i = 0; i < checkDeck.getSIZE_OF_DECK(); i++) {
+            for (int cardIndex = 0; cardIndex < SIZE_OF_HAND; cardIndex++) {
+                if (checkDeck.peekCard(i).equals(this.hand[cardIndex])) {
+                    foundMissing = false;
+                    break;              // Stop looking for card
+                }
+            }
+            if (foundMissing && handsCompleted < totalPossibleHands) {
+                possibleHands[handsCompleted].hand[lastPosition] = checkDeck.peekCard(i);
+                handsCompleted++;
+            }
+            foundMissing = true;
+        }
+        return possibleHands;
+    }
+
+    private int calculateProbImprove(int greater, int totalPossible) {
+        float probImprovement = ((float) greater / totalPossible) * PROBABILITY_FACTOR;    // Probability calculated as fraction of possible hands that could improve card and then multiplied by PROBABILITY_FACTOR. This allows us to know what the maximum probability improvement score could be.
+        int probImprovementInt = (int) Math.round(probImprovement);
+        return probImprovementInt;
+    }
+
+    public int getDiscardProbability(int cardPosition) {
+        // Assumes hand improvements come in changing rank of hand (disregards changing rank of cards)
+        if (cardPosition < 0 || cardPosition > 4) {
+            return 0;
+        }
+        int handScore = this.getGameValue();
+        if (handScore > ROYAL_FLUSH_DFLT_VAL) {
+            return 0;
+        } else {
+            // Copy over all cards in hand to all the hands in the array (possibleHands) except for specified card
+            int totalPossibleHands = this.deck.getSIZE_OF_DECK() - SIZE_OF_HAND;
+            HandOfCards[] possibleHands = copyNonDiscardingCards(cardPosition, totalPossibleHands);
+
+            // Check deck to see which cards exist that aren't in hand. Put each of those in a different hand to complete all hands in possibleHands[][].
+            possibleHands = completeHandsWithDeck(possibleHands, totalPossibleHands);
+
+            // Loop through all hands in possibleHands and find scores for all.
+            // Take note of how many are greater in value than current hand (without discarding card).
+            int greaterScore = 0;
+            int lesserScore = 0;
+            int currentScore = this.getGameValue();
+            for (int i = 0; i < totalPossibleHands; i++) {
+                possibleHands[i].sort();    // Sort cards so their hand type can be correctly identified
+                if (possibleHands[i].getGameValue() > currentScore) {
+                    greaterScore++;
+                } else if (possibleHands[i].getGameValue() < currentScore) {
+                    lesserScore++;
+                }
+            }
+
+            // Calculate probability of improvement as ratio between possible hands that have a better score over those that have a worse score (all cards not in hand are equally likely to be found in deck).
+            int probImprovementInt = calculateProbImprove(greaterScore, totalPossibleHands);
+            return probImprovementInt;
+
+        }
+    }
+
 }
+
+
+
